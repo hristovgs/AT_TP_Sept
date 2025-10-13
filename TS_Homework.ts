fcf6284a16d4c5277514cbdcb678055c362884e2
hristovgs <hristovgs@gmail.com>
2025-10-13 11:57:08 +0300

    TS_Homework.ts file added

---
+++ TS_Homework.ts
@@ -0,0 +1,26 @@
+//TASK1 
+// Declare variables
+let name: string = "Georgi Hristov";
+let age: number = 36;
+
+// Print using string interpolation
+console.log(`Hello! I am ${name}, ${age} years old`);
+
+
+//TASK2
+// Declare variables
+let num1: number = 25;
+let num2: number = 23.5;
+let sum : number = num1 + num2;
+let substr : number = num1 - num2;
+
+// Print addition and substruction results
+console.log(`Sum of ${num1} and ${num2} is ${sum}, and the difference is ${substr}.`);
+
+//TASK3
+// Declare variables
+let num3: number = 7.6;
+let isWithinRange : boolean = num3>=1||num3<=10;
+
+// Compare number
+console.log(isWithinRange);
